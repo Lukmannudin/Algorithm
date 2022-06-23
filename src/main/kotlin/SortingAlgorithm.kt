62863dcda34d0da4c1320c61c328797370007150
Lukmannudin <lukmannudin.priatna@happyfresh.com>
2022-06-24 02:40:42 +0700

    add integer quicksort

--- src/main/kotlin/SortingAlgorithm.kt
+++ src/main/kotlin/SortingAlgorithm.kt
@@ -1,113 +1,70 @@
 object SortingAlgorithm {
 
-//    lateinit var array: IntArray
+    lateinit var array: IntArray
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val list = mutableListOf(33, 10, 15, 7)
-        val sortedList = quickSort(list, 0)
+        val list = intArrayOf(3, -1)
+//
+//        this.array = list
+//        quickSort(0, array.lastIndex)
+//        val sortedList = quickSort(list, 0)
 
-        sortedList.forEach { i ->
+        array = list
+        val sorted = sortArray(list)
+        sorted.forEach { i ->
             print("$i ")
         }
     }
 
-    fun quickSort(list: MutableList<Int>, pivotIndex: Int): List<Int> {
-        val leftArray = mutableListOf<Int>()
-        val rightArray = mutableListOf<Int>()
+    fun sortArray(nums: IntArray): IntArray {
+        array = nums
+        quickSort(0, nums.lastIndex)
+        return array
+    }
 
-        if (list.size < 2) {
-            return list
+    private fun quickSort(left: Int, right: Int) {
+        var leftIndex = left
+        var rightIndex = right
+
+        val size = (right - left) + 1
+
+        // base case
+        if (size < 2) {
+            return
         }
 
-        if (list.size == 2) {
-            return if (list[0] > list[1]) {
-                swap(list, 0, 1)
-            } else {
-                list
+        if (size == 2) {
+            if (array[leftIndex] > array[rightIndex]) {
+                swap(leftIndex, rightIndex)
             }
         }
 
-        val pivot = list[pivotIndex]
-        list.removeAt(pivotIndex)
+        val pivotIndex = right
+        val pivot = array[pivotIndex]
 
-        list.forEach { number ->
-            if (number <= pivot) {
-                leftArray.add(number)
+        while (leftIndex < rightIndex) {
+            if (array[leftIndex] <= pivot) {
+                leftIndex++
+            } else if (array[rightIndex] >= pivot) {
+                rightIndex--
             } else {
-                rightArray.add(number)
+                swap(leftIndex, rightIndex)
             }
         }
 
-        return quickSort(leftArray, 0) + pivot + quickSort(rightArray, 0)
+        swap(leftIndex, pivotIndex)
+
+        val newPivot = leftIndex
+
+        quickSort(left, newPivot - 1)
+        quickSort(newPivot + 1, right)
     }
 
-    fun swap(list: MutableList<Int>, firstIndex: Int, lastIndex: Int): MutableList<Int> {
-        val dump = list[firstIndex]
-        list[firstIndex] = list[lastIndex]
-        list[lastIndex] = dump
-        return list
+    fun swap(firstIndex: Int, secondIndex: Int) {
+        val dump = array[firstIndex]
+        array[firstIndex] = array[secondIndex]
+        array[secondIndex] = dump
     }
-
-
-//    fun sortArray(nums: IntArray): IntArray {
-//        this.array = nums
-//        quickSort(0, array.size - 1)
-//        return array
-//    }
-//
-//    fun quickSort(min: Int, max: Int) {
-//        if (max - min == 2) {
-//            if (array[max] < array[min]) {
-//                swap(max, min)
-//            }
-//            return
-//        }
-//
-//        if (max - min < 2) {
-//            return
-//        }
-//
-//        val pivotIndex = max
-//        val pivot = array[pivotIndex]
-//
-//        var leftPointer = min
-//        var rightPointer = max - 1
-//
-//        while (leftPointer < rightPointer) {
-//            val currentLeft = array[leftPointer]
-//            val currentRight = array[rightPointer]
-//
-//            if (currentLeft < pivot) {
-//                leftPointer++
-//            } else if (currentRight > pivot) {
-//                rightPointer--
-//            } else {
-//                swap(leftPointer, rightPointer)
-//            }
-//        }
-//
-//        val mid = leftPointer // also same for right pointer
-//        swap(mid, pivotIndex)
-//
-//        println(subArray(0, mid).contentToString())
-//        println(subArray(mid + 1, pivotIndex).contentToString())
-//        println()
-//
-//        quickSort(0, mid)
-//        quickSort(mid + 1, pivotIndex)
-//    }
-//
-//    fun swap(leftIndex: Int, rightIndex: Int) {
-//        val dump = array[leftIndex]
-//        array[leftIndex] = array[rightIndex]
-//        array[rightIndex] = dump
-//    }
-//
-//    fun subArray(leftIndex: Int, rightIndex: Int): IntArray {
-//        return (leftIndex..rightIndex).map {
-//            array[it]
-//        }.toIntArray()
-//    }
 
 }
